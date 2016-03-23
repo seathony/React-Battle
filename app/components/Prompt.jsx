59c94f3839bf20198a2779a8cf43d7590684395a
sean <seathony@gmail.com>
2016-03-23 13:34:32 -0400

    ui in stateless funtion

--- app/components/Prompt.jsx
+++ app/components/Prompt.jsx
@@ -4,44 +4,42 @@ import TextField from 'material-ui/lib/text-field';
 import RaisedButton from 'material-ui/lib/raised-button';
 let PropTypes = React.PropTypes;
 
-export default class Prompt extends React.Component {
-  static propTypes = {
-    header: PropTypes.string.isRequired,
-    onUpdateUser: PropTypes.func.isRequired,
-    onSubmitUser: PropTypes.func.isRequired,
-    username: PropTypes.string.isRequired,
-
-  }
-
-  render() {
-    return (
-      <Grid>
-        <Row center='xs'>
-          <Col xs={4} >
-            <h1> {this.props.header} </h1>
-          <Col xs={4}/>
-          <form onSubmit={this.props.onSubmitUser.bind(this)}>
-            <div>
-              <TextField
-                hintText='GitHub username'
-                floatingLabelText='Enter name'
-                onChange={this.props.onUpdateUser.bind(this)}
-                value={this.props.username}
-                type='text'
-                />
-            </div>
-          <Col xs={4}/>
-            <div>
-              <RaisedButton
-              label='Click me'
-              secondary={true}
-              type='submit'
+export default function Prompt(props) {
+  return (
+    <Grid>
+      <Row center='xs'>
+        <Col xs={4} >
+          <h1> {props.header} </h1>
+        <Col xs={4}/>
+        <form onSubmit={props.onSubmitUser}>
+          <div>
+            <TextField
+              hintText='GitHub username'
+              floatingLabelText='Enter name'
+              onChange={props.onUpdateUser}
+              value={props.username}
+              type='text'
               />
-            </div>
-            </form>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+          </div>
+        <Col xs={4}/>
+          <div>
+            <RaisedButton
+            label='Click me'
+            secondary={true}
+            type='submit'
+            />
+          </div>
+          </form>
+        </Col>
+      </Row>
+    </Grid>
+  );
 }
+
+Prompt.propTypes = {
+  header: PropTypes.string.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  onSubmitUser: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+
+};
--- webpack.config.js
+++ webpack.config.js
@@ -12,40 +12,44 @@ const common = {
   // Entry accepts a path or an object of entries
   // The build chapther contains an example of the latter
   entry: PATHS.app,
+
   // Add resolve.extensions.
   // '' is needed to allow imports without an extension.
   // Note the .'s before extension as it will fail to match without!!
   resolve: {
-    extensions:['', '.js', '.jsx']
+    extensions:['', '.js', '.jsx'],
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-  module: { 
+  module: {
     loaders: [
     {
       // Test expects a RegExp! Note the slashes!
       test: /\.css$/,
       loaders: ['style', 'css?modules', 'sass'],
+
       // Include accepts either a path or an array of paths.
-      include: [PATHS.app, __dirname,'./node_modules/react-flexbox-grid'],
+      include: [PATHS.app, __dirname, './node_modules/react-flexbox-grid'],
     },
+
     // Set up jsx. This accepts js too thanks to RegExp
     {
       test: /\.jsx?$/,
+
       // enable caching for improved performance during development
       // It uses default OS directory by default. If you need somehting
       // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
       loaders: ['babel?cacheDirectory'],
       include: PATHS.app,
-    }
-   ]
-  }
+    },
+  ],
+  },
 };
 
 // Default confiuration
-if(TARGET === 'start' || !TARGET) {
+if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
     devServer: {
@@ -58,21 +62,20 @@ if(TARGET === 'start' || !TARGET) {
       hot: true,
       inline: true,
       progress: true,
-      
+
       // Display only errors to reduce the amount of output.
       stats: 'errors-only',
-      
+
       // Parse host and port from env so this is easy to customize.
       host: process.env.HOST,
       port: process.env.PORT,
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ]
+      new webpack.HotModuleReplacementPlugin(),
+    ],
   });
 }
 
-if(TARGET === 'build') {
+if (TARGET === 'build') {
   module.exports = merge(common, {});
 }
-
